b3f6b523bb5fe50aaa61ad6b54953c342a8f1b67
thansih-ahammed <thansihahammed7356@gmail.com>
2023-06-13 18:25:57 +0530

    start

---
+++ src/components/includes/SignUp.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function SignUp() {
+  return (
+    <section className="p-3 flex justify-center mx-auto ">
+        <div   >
+
+        </div>
+        <div className="right">
+            <form action="">
+
+            </form>
+        </div>
+    </section>
+  )
+}
