85604b450e1bb6c6d481784dca31954128c8f93b
thansih-ahammed <thansihahammed7356@gmail.com>
2023-06-21 17:21:19 +0530

    fix:eroors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
 import { createContext, useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import SignUp from "./components/includes/SignUp";
 import NoMatch from "./components/includes/NoMatch";
 import Login from "./components/includes/Login";
@@ -31,26 +36,27 @@ function App() {
     setUserData(JSON.parse(localStorage.getItem("user_data")));
     setLoading(false);
   }, []);
- return loading ? (
-   <PreLoader/>
-
- ): (
+  return loading ? (
+    <PreLoader />
+  ) : (
     <>
-         <UserContext.Provider value={{ userData, updateUserData }}>
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={<Navigate to="/sign-up/auth/login/" element={<Login />} />}
-        />
-        <Route path="/sign-up/auth/create" element={<SignUp />} />
-        <Route path="/sign-up/auth/login" element={<Login />} />
-        <Route path="/sign-up/main" element={<Register />} />
+      <UserContext.Provider value={{ userData, updateUserData }}>
+        <Router>
+          <Routes>
+            <Route
+              path="sign-up/"
+              element={
+                <Navigate to="auth/login/" element={<Login />} />
+              }
+            />
+            <Route exact path="sign-up/auth/create" element={<SignUp />} />
+            <Route exact path="sign-up/auth/login" element={<Login />} />
+            <Route exact path="sign-up/main" element={<Register />} />
 
-        <Route path="*" element={<NoMatch />} />
-      </Routes>
-    </Router>
-          </UserContext.Provider>
+            <Route path="*" element={<NoMatch />} />
+          </Routes>
+        </Router>
+      </UserContext.Provider>
     </>
   );
 }
